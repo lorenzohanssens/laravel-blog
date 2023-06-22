d5c392aa5582d9ad312406a83b8ca0d36588c2f2
Lorenzo Hanssens <lorenzo.hanssens@gmail.com>
2023-06-22 17:46:50 +0200

    Section 5 - 25...27 - Post creation & md support

---
+++ app/Http/Controllers/PostController.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Post;
+use Illuminate\Support\Str;
+use Illuminate\Http\Request;
+
+class PostController extends Controller
+{
+    //
+    public function showCreateForm() {
+        return view("create-post");
+    }
+    public function storeNewPost(Request $request) {
+        $incomingFields = $request->validate([
+            'title' => 'required',
+            'body' => 'required'
+        ]);
+        $incomingFields['title'] = strip_tags($incomingFields['title']);
+        $incomingFields['body'] = strip_tags($incomingFields['body']);
+        $incomingFields['user_id'] = auth()->id();
+        $newPost = Post::create($incomingFields);
+        return redirect("/post/{$newPost->id}")->with('success', "Successfully created!");
+    }
+
+    public function viewSinglePost(Post $id) {
+        //markdown
+        $id['body'] = strip_tags(Str::markdown($id->body), "<a><p><ul><ol><li><strong><em><h1><h2><h3><hr><br>");
+return view('single-post', ['post' => $id]);
+    }
+}
