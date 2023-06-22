1f864272880b452da388bc4375fc5cdb1f9b99a7
Lorenzo Hanssens <lorenzo.hanssens@gmail.com>
2023-06-22 15:20:47 +0200

    Section 4  - Basic auth

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,6 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\ExampleController;
 use App\Http\Controllers\UserController;
 /*
 |--------------------------------------------------------------------------
@@ -14,8 +13,9 @@ use App\Http\Controllers\UserController;
 |
 */
 
-Route::get('/', [ExampleController::class, "homepage"]);
+Route::get('/', [UserController::class, "showCorrectHomepage"]);
 
-Route::get('/about', [ExampleController::class, "aboutpage"]);
 
 Route::post('/register', [UserController::class, "register"]);
+Route::post('/login', [UserController::class, "login"]);
+Route::post('/logout', [UserController::class, "logout"]);
