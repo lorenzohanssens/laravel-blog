1d7792dd7aa1c7adcf373c53132017c0ff289ef5
Lorenzo Hanssens <lorenzo.hanssens@gmail.com>
2023-06-27 11:12:55 +0200

    Section 5 - 31...32 - Policies

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -9,25 +9,59 @@ use Illuminate\Http\Request;
 class PostController extends Controller
 {
     //
-    public function showCreateForm() {
+    public function showCreateForm()
+    {
         //could check if user has auth, if so return redirect to homepage but that's not the way to go about it. middleware.
         return view("create-post");
     }
-    public function storeNewPost(Request $request) {
+    public function storeNewPost(Request $request)
+    {
+        $incomingFields = $request->validate([
+            "title" => "required",
+            "body" => "required",
+        ]);
+        $incomingFields["title"] = strip_tags($incomingFields["title"]);
+        $incomingFields["body"] = strip_tags($incomingFields["body"]);
+        $incomingFields["user_id"] = auth()->id();
+        $newPost = Post::create($incomingFields);
+        return redirect("/post/{$newPost->id}")->with(
+            "success",
+            "Successfully created!"
+        );
+    }
+
+    public function viewSinglePost(Post $id)
+    {
+        //markdown
+        $id["body"] = strip_tags(
+            Str::markdown($id->body),
+            "<a><p><ul><ol><li><strong><em><h1><h2><h3><hr><br>"
+        );
+        return view("single-post", ["post" => $id]);
+    }
+
+    public function delete(Post $post)
+    {
+        $post->delete();
+        return redirect("/profile/" . auth()->user()->username)->with(
+            "success",
+            "Post successfully deleted"
+        );
+    }
+
+    public function showEditForm(Post $post)
+    {
+        return view("edit-post", ["post" => $post]);
+    }
+    public function updatePost(Post $post, Request $request)
+    {
         $incomingFields = $request->validate([
             'title' => 'required',
             'body' => 'required'
         ]);
-        $incomingFields['title'] = strip_tags($incomingFields['title']);
-        $incomingFields['body'] = strip_tags($incomingFields['body']);
-        $incomingFields['user_id'] = auth()->id();
-        $newPost = Post::create($incomingFields);
-        return redirect("/post/{$newPost->id}")->with('success', "Successfully created!");
-    }
-
-    public function viewSinglePost(Post $id) {
-        //markdown
-        $id['body'] = strip_tags(Str::markdown($id->body), "<a><p><ul><ol><li><strong><em><h1><h2><h3><hr><br>");
-return view('single-post', ['post' => $id]);
+        $incomingFields["title"] = strip_tags($incomingFields["title"]);
+        $incomingFields["body"] = strip_tags($incomingFields["body"]);
+        $post->update($incomingFields);
+        return back()->with('success', "Done!");
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -24,6 +24,9 @@ Route::post('/logout', [UserController::class, "logout"])->middleware('mustBeLog
 Route::get('/create-post', [PostController::class, "showCreateForm"])->middleware('mustBeLoggedIn');
 Route::post('/create-post', [PostController::class, "storeNewPost"])->middleware('mustBeLoggedIn');
 Route::get('/post/{id}', [PostController::class, "viewSinglePost"]);
+Route::delete('/post/{post}', [PostController::class, "delete"])->middleware('can:delete,post');
+Route::get('/post/{post}/edit', [PostController::class, "showEditForm"])->middleware('can:update,post');
+Route::put('/post/{post}', [PostController::class, "updatePost"])->middleware("can:update,post");
 
 //profile related routes
 Route::get('/profile/{user:username}', [UserController::class, "profile"]); //7:17 ep 30
